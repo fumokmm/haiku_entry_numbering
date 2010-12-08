dd63a06844b0963a7df6da1eff204983743e74b1
fumokmm <fumo.kmm@gmail.com>
2010-12-08 21:58:47 +0900

    途中までのクラス実装とTODOコメントを付加(まだ動かない)

--- haiku_entry_numbering.user.js
+++ haiku_entry_numbering.user.js
@@ -8,12 +8,70 @@
 // @version        0.01
 // ==/UserScript==
 
+// TODO: キーワードタイムライン以外も対応
+
 (function() {
+    function Queue() {
+	this._a = []
+	this.enqueue = function(o) {
+	    this._a.push(o)
+	}
+	this.dequeue = function() {
+	    if (this._a.length > 0) {
+		return this._a.shift()
+	    }
+	    return null
+	}
+	this.size = function() {
+	    return this._a.length
+	}
+ 	this.contains = function(o) {
+	    var s = this.size()
+	    for (var i = 0; i < s; i++) {
+		if (this._a[i] == o) return true
+	    }
+	    return false
+	}
+	this.toString = function() {
+	    return '[' + this._a.join(', ') + ']'
+	}
+    }
+
+    var DataStore = function() {
+	this.limit = 100
+	this.list = new Queue()
+	this.map = {}
+	this.update = function(key, value) {
+	    if (this.list.contains(key)) {
+		if (this.map[key] != value) {
+		    this.map[key] = value
+		}
+	    } else {
+		if (this.list.size() == this.limit) {
+		    var deletedKey = this.list.dequeue()
+		    delete this.map[deletedKey]
+		}
+		this.list.enqueue(key)
+		this.map[key] = value
+	    }
+	}
+	this.get = function(key) {
+	    
+	}
+    }
+
+
+
+
+
+
     // --------------------------------------------------------------
     // 定数定義
 
     // IDのプレフィックス
     var NUMBER_TEMPLATE = '$num: ';
+
+    // TODO: データストアからデータを取得
 
     // 現在のキーワード
     var searchKeywordInfo = getKeywordInfo()
@@ -65,9 +123,17 @@
 	nodes.forEach(function(node){
             // タイトルのh2要素を取得
             var titles = xpath("descendant-or-self::div[@class='entry']/div[@class='list-body']/h2[@class='title']", node)
+	    // TODO: ステータスIDも取得
 	    titles.forEach(function(titleNode) {
 		// number が nowNumber 以下になったら実行
 		if (number <= searchKeywordInfo.nowNumber) {
+		    // TODO: データストアから取得できたらその値を使う
+		    // if (dataStore.contains(statusId)) {
+                    //     dataStore.update(statusId, number)
+		    //     var num = dataStore.get(statusId)
+		    // }
+		    // TODO: ただし更新してから(最新の番号優先) ⇒データストアに保存
+		    // TODO: 取得できなかったら、現在の値を反映して番号を付ける
 	            // 番号のspan要素を生成
                     var numberNode = document.createElement('span')
                     numberNode.appendChild(document.createTextNode(NUMBER_TEMPLATE.replace('$num', number)))
@@ -79,6 +145,7 @@
 	})
 
         searchKeywordInfo.nowNumber = number
+	// TODO: データストアに現在のデータの状態を反映
     }
 
     /**
