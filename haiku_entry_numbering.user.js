865a3f7ab859ae86e6f5fde724d94a9ec32e2453
fumokmm <fumo.kmm@gmail.com>
2010-12-11 17:06:32 +0900

    できたかも

--- haiku_entry_numbering.user.js
+++ haiku_entry_numbering.user.js
@@ -2,7 +2,7 @@
 // @name           haiku_entry_numbering
 // @namespace      http://d.hatena.ne.jp/fumokmm/
 // @description    haiku_entry_numbering
-// @include        http://h.hatena.ne.jp/keyword/*
+// @include        http://h.hatena.ne.jp/*
 // @author         fumokmm
 // @date           2010-12-08
 // @version        0.01
@@ -23,57 +23,64 @@
 
     // データストアからデータを取得
     var dataStore = eval('(' + GM_getValue(DS_KEY, new DataStore().toSource()) + ')')
-    dataStore.update("k" + dataStore.size(), "v" + dataStore.size())
-    alert(dataStore.getValue('k5'))
-    GM_setValue(DS_KEY, dataStore.toSource())
 
     // 現在のキーワード
     var searchKeywordInfo = getKeywordInfo()
-    searchKeywordInfo.nowNumber = -1
     var apiInfo = null
-    // API呼び出し
-    GM_xmlhttpRequest({
-        method: 'GET',
-        url   : 'http://h.hatena.ne.jp/api/keywords/show/' + searchKeywordInfo.keyword + '.json',
-        onload: function(httpObj) {
-	    /*======= NOTE ====== [JSON sample] ==
-	    {
-	      "related_keywords" : [
-	        "xxx",
-	        "yyy"
-	      ],
-	      "link" : "http://h.hatena.ne.jp/keyword/zzz",
-	      "followers_count" : "1",
-	      "title" : "zzz",
-	      "entry_count" : "213"
-	    }
-	    ======================================*/
-	    apiInfo = eval('(' + httpObj.responseText + ')')
-            //メイン処理を実行
-	    main([document], apiInfo)
-        }
-    })
+    var callMain = null
+
+    if (searchKeywordInfo) {
+        searchKeywordInfo.nowNumber = -1
+        // API呼び出し
+        GM_xmlhttpRequest({
+            method: 'GET',
+            url   : 'http://h.hatena.ne.jp/api/keywords/show/' + searchKeywordInfo.keyword + '.json',
+            onload: function(httpObj) {
+                /*======= NOTE ====== [JSON sample] ==
+	        {
+	          "related_keywords" : [
+	            "xxx",
+	            "yyy"
+	          ],
+	          "link" : "http://h.hatena.ne.jp/keyword/zzz",
+	          "followers_count" : "1",
+	          "title" : "zzz",
+	          "entry_count" : "213"
+	        }
+	        ======================================*/
+                apiInfo = eval('(' + httpObj.responseText + ')')
+                //メイン処理を実行
+                mainForKeyword([document], apiInfo)
+            }
+        })
+    } else {
+        callMain = function(){ main([document]) }
+    }
+
+    if (callMain) {
+        callMain()
+    } else {
+        callMain = function(){ mainForKeyword([document], apiInfo) }
+    }
 
     // AutoPagerizeで継ぎ足されたページでもメイン処理を実行できるように登録
     // (by http://os0x.g.hatena.ne.jp/os0x/20080131/1201762604)
     setTimeout(function() {
 	if (window.AutoPagerize && window.AutoPagerize.addFilter) {
-	    window.AutoPagerize.addFilter(function(){ main([document], apiInfo) })
+	    window.AutoPagerize.addFilter(callMain)
 	} else {
 	    window.addEventListener('GM_AutoPagerizeLoaded', function(){
-		window.AutoPagerize.addFilter(function(){ main([document], apiInfo) })
+		window.AutoPagerize.addFilter(callMain)
 	    }, false)
 	}
     }, 0)
 
     /**
-     * メイン処理
+     * メイン処理(キーワードページ用)
      */
-    function main(nodes, apiInfo) {
+    function mainForKeyword(nodes, apiInfo) {
         // 現在のキーワードのエントリ数
         var number = apiInfo.entry_count - (searchKeywordInfo.page - 1) * 20
-	if (searchKeywordInfo.nowNumber == -1) searchKeywordInfo.nowNumber = number
-
 	nodes.forEach(function(node){
             // タイトルのh2要素を取得
             var titles = xpath("descendant-or-self::div[@class='entry']/div[@class='list-body']/h2[@class='title']", node)
@@ -81,36 +88,57 @@
 　　　　　　      // ステータスIDを取得
                 var infoNode = xpath("descendant::div[@class='info']/span[@class='timestamp']", titleNode.parentNode)[0]
                 var statusId = infoNode.firstChild.href.replace(/^.+\//, '')
+                // データストアに情報を設定
+                dataStore.update(statusId, number)
 
-		// number が nowNumber 以下になったら実行
-		if (number <= searchKeywordInfo.nowNumber) {
-		    // TODO: データストアから取得できたらその値を使う
-		    // if (dataStore.contains(statusId)) {
-                    //     dataStore.update(statusId, number)
-		    //     var num = dataStore.get(statusId)
-		    // }
-		    // TODO: ただし更新してから(最新の番号優先) ⇒データストアに保存
-		    // TODO: 取得できなかったら、現在の値を反映して番号を付ける
+                // まだ追加していなければ実行
+                if (titleNode.firstChild.tagName.toLowerCase() != 'span') {
 	            // 番号のspan要素を生成
                     var numberNode = document.createElement('span')
                     numberNode.appendChild(document.createTextNode(NUMBER_TEMPLATE.replace('$num', number)))
-
                     // 生成した番号のspan要素をタイトルの前に追加
                     titleNode.insertBefore(numberNode, titleNode.firstChild)
 	        }
                 number--
             })
 	})
+        // データストア保存
+        GM_setValue(DS_KEY, dataStore.toSource())
+    }
 
-        searchKeywordInfo.nowNumber = number
-	// TODO: データストアに現在のデータの状態を反映
+    /**
+     * メイン処理(キーワードページ以外)
+     */
+    function main(nodes) {
+	nodes.forEach(function(node){
+            // タイトルのh2要素を取得
+            var titles = xpath("descendant-or-self::div[@class='entry']/div[@class='list-body']/h2[@class='title']", node)
+	    titles.forEach(function(titleNode) {
+　　　　　　      // ステータスIDを取得
+                var infoNode = xpath("descendant::div[@class='info']/span[@class='timestamp']", titleNode.parentNode)[0]
+                var statusId = infoNode.firstChild.href.replace(/^.+\//, '')
+                var number = dataStore.getValue(statusId)
+                if (number) {
+                    // まだ追加していなければ実行
+	            if (titleNode.firstChild.tagName.toLowerCase() != 'span') {
+	                // 番号のspan要素を生成
+                        var numberNode = document.createElement('span')
+                        numberNode.appendChild(document.createTextNode(NUMBER_TEMPLATE.replace('$num', number)))
+                        // 生成した番号のspan要素をタイトルの前に追加
+                        titleNode.insertBefore(numberNode, titleNode.firstChild)
+	            }
+                }
+            })
+	})
     }
 
     /**
      * キーワード情報を取得
-     * @return [keyword: キーワード, page: ページ番号]
+     * @return キーワードページの場合   : [keyword: キーワード, page: ページ番号]
+     *         キーワードページ以外の場合: undefined
      */
     function getKeywordInfo() {
+        if (location.pathname.search('/keyword') < 0) return null
         var qsMap = null
         var qs = location.search.substr(1)
         if (qs) {
@@ -149,7 +177,7 @@
 
     // --------------------------------------------------------------
     function DataStore() {
-        this._limit = 5
+        this._limit = 1000
 	this._a = []
         this._m = {}
 
